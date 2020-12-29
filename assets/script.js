10aee620a25086157136d3e9195cc1afa958c5a2
Jas James <jasdjames@gmail.com>
2020-12-29 15:33:50 -0600

    added local time to page using luxon CDN

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,20 @@
 var DateTime = luxon.DateTime;
-const currentTime = DateTime.local();
-console.log(currentTime);
+var dt = DateTime.local();
+var Day = $("#currentDay")
+var newFormat = Day.text(dt.toLocaleString(DateTime.DATE_HUGE)); 
+
+console.log(dt);
+$(".btn-outline-secondary").addClass("saveBtn");
+$(".input-group-prepend").addClass("row");
+$(".input-group-text").addClass("hour time-block");
+$("textarea.form-control").addClass("hour");
+
+// If the hours equals now = give x class 
+   //   heres where I was working -----> if $(dt.startOf('hour')) - 
+
+// if the hour is past give y class 
+
+// if this hour is the future give z class --- Check luxon documentation
 
 
-var save = $(".btn-outline-secondary").addClass("saveBtn");
-var rowS = $(".input-group-prepend").addClass("row");
-var hourS = $(".input-group-text").addClass("hour time-block");
-var innerText = $("textarea.form-control").addClass("hour");
 
