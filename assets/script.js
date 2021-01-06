6a879d085d9ec269521ed499aed18c2a4d40fa89
Jas James <jasdjames@gmail.com>
2021-01-05 22:40:59 -0600

    spilled water on MacBook- switching computers to let dry

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,23 @@
-var DateTime = luxon.DateTime;
-var dt = DateTime.local();
-var Day = $("#currentDay")
-var newFormat = Day.text(dt.toLocaleString(DateTime.DATE_HUGE));
+$(document).ready(function () {
 
-
-    
-
-console.log(dt.hour);
-console.log($("textarea.form-control").attr("data-hr"))
-
-$(".btn-outline-secondary").addClass("saveBtn");
-$(".input-group-prepend").addClass("row");
-$(".input-group-text").addClass("hour time-block");
-var textBox = $("textarea.form-control").addClass("hour");
+    var DateTime = luxon.DateTime;
+    var dt = DateTime.local();
+    var Day = $("#currentDay")
+    var newFormat = Day.text(dt.toLocaleString(DateTime.DATE_HUGE));
+    console.log(dt.hour);
+    console.log($("textarea.form-control").attr("data-hr"))
+    $(".btn-outline-secondary").addClass("saveBtn");
+    $(".input-group-prepend").addClass("row");
+    $(".input-group-text").addClass("hour time-block");
+    var textBox = $("textarea.form-control").addClass("hour");
 
 
 
-// I was helped by my instructor with this block and how to make it  work correctly during  office hours
+    // I was helped by my instructor with this block and how to make it  work correctly during  office hours
 
-$('textarea.form-control').each(function(el){
-    console.log($(this).attr('data-hr'));
-    console.log (parseInt($(this).attr('data-hr')));
+    $('textarea.form-control').each(function (el) {
+        console.log($(this).attr('data-hr'));
+        console.log(parseInt($(this).attr('data-hr')));
         if (dt.hour === parseInt($(this).attr('data-hr'))) {
             $(this).addClass("present");
         } else if (dt.hour > $(this).attr('data-hr')) {
@@ -28,19 +25,28 @@ $('textarea.form-control').each(function(el){
         } else {
             $(this).addClass("future");
         }
-        })  
+
+
+    });
+
+
+
     
+});
+    var userText = $('textarea.form-control').val()
+    $(".saveBtn").on("click", function (event) {
+        
+        
 
+        if ($(this)) {
+            console.log($(this));
+            console.log(userText);
+            $('textarea.form-control').each(function (el) {
+            localStorage.setItem("text", userText);
 
-
-
-$(".saveBtn").click(function saveRow(event) {
-
-    localStorage.setItem("text".textBox.innerText);
-
-
-})
-
+        })
+    }
+    })
 
 
 
